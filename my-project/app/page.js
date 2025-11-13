2b77be5bf6616746afda0c79bd4a732a4c6d6fad
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-11-13 19:46:35 +0530

    added intial file

--- my-project/app/page.js
+++ my-project/app/page.js
@@ -2,8 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold ">
-      Hello world!
-    </h1>
+    <div>
+    </div>
   );
 }
