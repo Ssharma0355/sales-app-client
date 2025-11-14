af0cd34b8a3935d98ec1bec606b696d055abd13b
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-11-14 09:20:08 +0530

    header layout

---
+++ my-project/app/page/LandingPage.jsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Header />
+      <h1>Landing Page</h1>
+
+    </div>
+  )
+}
+
+export default LandingPage
